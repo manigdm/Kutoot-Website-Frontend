939e86c597b0e6439b9c746f0810e5d7f0234d71
Manikandan Moorthi <manikandan@mantralabsglobal.com>
2025-08-20 14:02:02 +0530

    reverted old state

--- app/(pages)/campaignpage/page.jsx
+++ app/(pages)/campaignpage/page.jsx
@@ -1,12 +1,9 @@
-import { Suspense } from "react";
 import Campaign from "@/components/campaignpage/campaign";
 const page = () => {
   return (
-  	<Suspense fallback={<div>Loading...</div>}>
 	    <div>
 	      <Campaign/>
 	    </div>
-    </Suspense>
   )
 }
 export default page
